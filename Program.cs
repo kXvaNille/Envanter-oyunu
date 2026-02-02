69e0614a222cce599b51d85e0923cfa89a75b333
kXvaNille <kayra9866@gmail.com>
2026-02-02 23:11:18 +0100

    Mini dungeon game

--- Program.cs
+++ Program.cs
@@ -8,14 +8,42 @@ class Program
     static void Main(string[] args)
     {
 
-        for (int i = 0; i < 5; i++)
+        int health = 100;
+        int gold = 0;
+
+        List<string> dungeon = new List<string>();
+
+        dungeon.Add("Empty Room");
+        dungeon.Add("Enemy");
+        dungeon.Add("Treasure");
+        dungeon.Add("Trap");
+        dungeon.Add("Enemy");
+
+        foreach (string oda in dungeon)
         {
-            for (int j = 0; j < 5; j++)
+
+            if (oda == "Enemy")
             {
-                Console.Write("*");
+                health -= 20;
+                Console.WriteLine("An enemy appeared! You took damage.");
             }
-            Console.WriteLine();
-        } 
+            else if (oda == "Treasure")
+            {
+                gold += 50;
+                Console.WriteLine("You found a treasure chest! Gold added.");
+            }
+            else if (oda == "Trap")
+            {
+                health -=10;
+                Console.WriteLine("You stepped on a trap! Health decreased.");
+            }
+            else
+            {
+                Console.WriteLine("The room is clear. Moving on...");
+            }
+            
+        }
+        Console.WriteLine("Status -> HP: " + health + " | Gold: " + gold);
 
     }
 }
